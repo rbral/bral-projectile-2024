55889acf8de244a2c5515a0839effe17bc60fabe
rbral <rbral@student.touro.edu>
2024-02-13 22:36:36 -0500

    edited checkstyles

--- src/main/java/bral/projectile/Projectile.java
+++ src/main/java/bral/projectile/Projectile.java
@@ -44,11 +44,14 @@ public class Projectile {
         return (Math.sin(radians) * velocity) * (Math.sin(radians) * velocity) / (9.8 * 2);
     }
 
+    /**
+     * @return the x-intercept
+     */
     public double getInterceptX() {
         // Assuming initial height (h) is zero for simplicity
         double t = (2 * velocity * Math.sin(radians)) / 9.8;  // Time of flight
-        double xIntercept = velocity * t * Math.cos(radians);  // Horizontal position at x-intercept
-        return xIntercept;
+        return velocity * t * Math.cos(radians);  // Horizontal position at x-intercept
+
     }
 
 
